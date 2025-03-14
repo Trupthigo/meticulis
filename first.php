617a6b56bc0a7311a20f5d24ec7ed5d0df7c3ce5
Trupthigo <mstrupthi9102@gmail.com>
2025-03-14 07:39:42 +0530

    about us2

---
+++ first.php
@@ -0,0 +1,67 @@
+<!-- about -->
+<link rel="stylesheet" href="../css/first.css">
+<div class="about">
+   <a class="bg_links social portfolio" href="https://www.rafaelalucas.com" target="_blank">
+      <span class="icon"></span>
+   </a>
+   <a class="bg_links social dribbble" href="https://dribbble.com/rafaelalucas" target="_blank">
+      <span class="icon"></span>
+   </a>
+   <a class="bg_links social linkedin" href="https://www.linkedin.com/in/rafaelalucas/" target="_blank">
+      <span class="icon"></span>
+   </a>
+   <a class="bg_links logo"></a>
+</div>
+<!-- end about -->
+
+<nav class="mainNav">
+   <div class="mainNavlogo">Logo</div>
+   <div class="mainNavlinks">
+      <a href="" class="mainNavlink">About</a>
+      <a href="" class="mainNavlink">Projects</a>
+      <a href="" class="mainNavlink">Team</a>
+      <a href="" class="mainNavlink">Contacts</a>
+   </div>
+   <div class="mainNavicon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+         <g data-name="Layer 2" fill="#9197AE">
+            <g data-name="menu-2">
+               <rect
+                  width="24"
+                  height="24"
+                  transform="rotate(180 12 12)"
+                  opacity="0"
+               />
+               <circle cx="4" cy="12" r="1" />
+               <rect x="7" y="11" width="14" height="2" rx=".94" ry=".94" />
+               <rect x="3" y="16" width="18" height="2" rx=".94" ry=".94" />
+               <rect x="3" y="6" width="18" height="2" rx=".94" ry=".94" />
+            </g>
+         </g>
+      </svg>
+   </div>
+</nav>
+<header class="mainHeading">
+   <div class="mainHeadingcontent">
+      <article class="mainHeadingtext">
+         <p class="mainHeadingpreTitle">nature</p>
+         <h2 class="mainHeadingtitle">behind the mountains</h2>
+         <p class="mainHeadingdescription">
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.
+         </p>
+         <button class="cta">know more</button>
+      </article>
+
+      <figure class="mainHeading__image">
+         <img
+            src="https://images.unsplash.com/photo-1520856707909-75c4048cc858?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
+            alt=""
+         />
+      </figure>
+   </div>
+</header>
+<script>
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+</script>
